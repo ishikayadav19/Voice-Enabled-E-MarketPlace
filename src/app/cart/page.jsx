e763dac546c7f37ba57a7380365cef310c234394
m-ishikayadav19-m <ishikasingh1912@gmail.com>
2025-03-18 19:27:38 +0530

    created cart page

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -276,7 +276,7 @@ const cart = () => {
         </div>
       </div>
       <button className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">
-        Check out
+        proceed
       </button>
     </div>
     {/* totals - end */}
