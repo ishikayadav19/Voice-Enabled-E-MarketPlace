11eeb106ef2218bc148eae66527299e87497965e
m-ishikayadav19-m <ishikasingh1912@gmail.com>
2025-03-19 13:08:57 +0530

    hero section

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -8,46 +8,232 @@ const Home = () => {
   <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
     <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
 
-      <section className="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48">
+      <section className=" relative flex flex-1 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-gray-100 py-16 shadow-lg h-screen md:py-20 xl:py-48">
         {/* image - start */}
         <img
-          src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500"
-          loading="lazy"
-          alt="Photo by Fakurian Design"
-          className="absolute inset-0 h-full w-full object-cover object-center"
-        />
+  src="/banner.jpeg"
+  alt="Voice Shopping"
+  className="absolute inset-0 h-full w-full object-cover object-center"/>
+
         {/* image - end */}
-        {/* overlay - start */}
-        <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
-        {/* overlay - end */}
-        {/* text start */}
-        <div className="relative flex flex-col items-center p-4 sm:max-w-xl">
-          <p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">
-            Very proud to introduce
-          </p>
-          <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">
-            Revolutionary way to build the web
-          </h1>
-          <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
-            <a
-              href="#"
-              className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
-            >
-              Start now
-            </a>
-            <a
-              href="#"
-              className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
-            >
-              Take tour
-            </a>
-          </div>
-        </div>
-        {/* text end */}
+      
       </section>
     </div>
   </div>
   {/* hero - end */}
+{/* collections start*/}
+
+
+  <div className="bg-white py-6 sm:py-8 lg:py-12">
+  <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <div className="mb-6 flex items-end justify-between gap-4">
+      <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+        Collections
+      </h2>
+      <a
+        href="#"
+        className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
+      >
+        Show more
+      </a>
+    </div>
+    <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by Austin Wade"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Men</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Business Causual
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by engin akyurt"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Women</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Summer Season
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by engin akyurt"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Women</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Summer Season
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by engin akyurt"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Women</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Summer Season
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by engin akyurt"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Women</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Summer Season
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by engin akyurt"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Women</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Summer Season
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1552668693-d0738e00eca8?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            loading="lazy"
+            alt="Photo by Austin Wade"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Men</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Streetwear
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+      {/* product - start */}
+      <div>
+        <a
+          href="#"
+          className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1560269999-cef6ebd23ad3?auto=format&q=75&fit=crop&w=600&h=700"
+            loading="lazy"
+            alt="Photo by Austin Wade"
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </a>
+        <div className="flex flex-col">
+          <span className="text-gray-500">Women</span>
+          <a
+            href="#"
+            className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+          >
+            Sale
+          </a>
+        </div>
+      </div>
+      {/* product - end */}
+    </div>
+  </div>
+</div>
+{/* collections end */}
   {/* gallery - start */}
   <div className="bg-white py-6 sm:py-8 lg:py-12">
     <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
